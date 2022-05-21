2c19ceac4731294412d25d7abe0957e54a1b7501
Marko Lohert <lohert.marko@gmail.com>
2022-05-21 11:02:53 +0200

    Demo app used in the presentation of  debugging multithreaded applications in Visual Studio 2022.

---
+++ Demo/DebuggingMultithreadedApp/DebuggingMultithreadedApp/Jobs.cs
@@ -0,0 +1,54 @@
+﻿namespace DebuggingMultithreadedApp;
+
+public class Jobs
+{
+    private string DataA { get; set; } = String.Empty;
+    private string DataB { get; set; } = String.Empty;
+
+    private const int minRnd = 100;
+    private const int maxRnd = 1000;
+    private const int stepsPerJob = 10;
+
+    public void JobA()
+    {
+        DataA = "JobA: Data A";
+        lock (DataA)
+        {
+            for (int i = 0; i < stepsPerJob; i++)
+            {
+                lock (DataB)
+                {
+                    DataB = "JobA: Data B";
+                    Console.WriteLine($"JobA: step = {i}");
+                }
+                Thread.Sleep(Random.Shared.Next(minRnd, maxRnd));
+            }
+        }
+    }
+
+    public void JobB()
+    {
+        DataB = "JobB: Data B";
+        lock (DataB)
+        {
+            for (int i = 0; i < stepsPerJob; i++)
+            {
+                lock (DataA)
+                {
+                    DataA = "JobB: Data A";
+                    Console.WriteLine($"JobB: step = {i}");
+                }
+                Thread.Sleep(Random.Shared.Next(minRnd, maxRnd));
+            }
+        }
+    }
+
+    public void JobC()
+    {
+        for (int i = 0; i < stepsPerJob; i++)
+        {
+            Console.WriteLine($"JobC: step = {i}");
+            Thread.Sleep(Random.Shared.Next(minRnd, maxRnd));
+        }
+    }
+}
---
+++ Demo/DebuggingMultithreadedApp/DebuggingMultithreadedApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using DebuggingMultithreadedApp;
+
+RunAllJobsOneByOne();
+//RunAllJobsParallel();
+//RunOnlyJobAIn4Threads();
+
+void RunAllJobsOneByOne()
+{
+    Jobs jobs = new();
+
+    jobs.JobA();
+
+    jobs.JobB();
+
+    jobs.JobC();
+}
+
+void RunAllJobsParallel()
+{
+    Jobs jobs = new();
+
+    Thread threadA = new(jobs.JobA);
+    threadA.Start();
+
+    Thread threadB = new(jobs.JobB);
+    threadB.Start();
+
+    Thread threadC = new(jobs.JobC);
+    threadC.Start();
+}
+
+void RunOnlyJobAIn4Threads()
+{
+    Jobs jobs = new();
+
+    Thread threadA1 = new(jobs.JobA);
+    threadA1.Start();
+
+    Thread threadA2 = new(jobs.JobA);
+    threadA2.Start();
+
+    Thread threadA3 = new(jobs.JobA);
+    threadA3.Start();
+
+    Thread threadA4 = new(jobs.JobA);
+    threadA4.Start();
+}
